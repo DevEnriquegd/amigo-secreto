4dd9377e966875eb3a90ccf5e61fe4327c59b2b0
Enrique Gonzalez <jgonzalezd1@upao.edu.pe>
2025-08-09 22:58:07 -0500

    Cree función añadirAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function añadirAmigo() {
+  let campoEntrada = document.getElementById("amigo");
+  let nuevoAmigo = campoEntrada.value.trim();
+
+  if (nuevoAmigo === "") {
+    alert("Por favor, inserte un nombre");
+    return;
+  }
+
+  amigos.push(nuevoAmigo);
+  limpiarCampoEntrada();
+}
+
+function limpiarCampoEntrada() {
+  document.getElementById("amigo").value = "";
+}
