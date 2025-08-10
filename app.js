c437183a8abaf8eb33c9b26d92e1f1cce4eafbe8
Enrique Gonzalez <jgonzalezd1@upao.edu.pe>
2025-08-09 23:07:59 -0500

    Cree función actualizarListaAmigos

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function añadirAmigo() {
+function agregarAmigo() {
   let campoEntrada = document.getElementById("amigo");
-  let nuevoAmigo = campoEntrada.value.trim();
-
+  let nuevoAmigo = campoEntrada.value;
   if (nuevoAmigo === "") {
     alert("Por favor, inserte un nombre");
     return;
@@ -12,6 +11,16 @@ function añadirAmigo() {
 
   amigos.push(nuevoAmigo);
   limpiarCampoEntrada();
+  actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+  }
 }
 
 function limpiarCampoEntrada() {
